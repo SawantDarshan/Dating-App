0b91090a0a53197f87fa24b5dc1e4cc77c34b53d
SawantDarshan <darshansawant1995@gmail.com>
2022-02-15 09:42:36 +0530

    new login page and added material ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { useState } from 'react';
-import './App.css';
-import Login from "./modules/pop_ups/login/Login"
-
+import { useState } from "react";
+import "./App.css";
+import Login from "./modules/pop_ups/login/Login";
 
 function App() {
-
-  let [user, setUser] = useState(false)
-  let toggle = ()=>setUser(!user)
+  let [user, setUser] = useState(false);
+  let toggle = () => setUser(!user);
 
   return (
     <div className="App">
-      {user && <Login/>}
+      {user && <Login />}
       <button onClick={toggle}>login</button>
     </div>
   );
