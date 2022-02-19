f172bf5bd691b4c4e649559ef869406ee492ebca
SawantDarshan <darshansawant1995@gmail.com>
2022-02-19 23:35:51 +0530

    login

--- src/router/RouterWrap.jsx
+++ src/router/RouterWrap.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../modules/pop_ups/login/Login";
+import Home from "../pages/Home"
 
 function RouterWrap() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route exact path="login" element={<Login />}></Route>
       </Routes>
     </Router>
