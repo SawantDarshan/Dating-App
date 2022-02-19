b20853f0039c8e9fa782c819cb8361623f881fcf
SawantDarshan <darshansawant1995@gmail.com>
2022-02-19 23:16:23 +0530

    routerWrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./App.css";
-<<<<<<< HEAD
-import RouterWrap from "./router/RouterWrap"
-=======
-import Login from "./modules/pop_ups/signUp/SignUp";
-
->>>>>>> 40e86fd93339ed14e030920937aa42047f74ad8e
+import RouterWrap from "./router/RouterWrap";
 
 function App() {
 
